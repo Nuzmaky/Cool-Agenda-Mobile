b521795f751b11110e7e80aa3f6a6e3de7456409
mquintinu <mateusq1@hotmail.com>
2015-05-25 13:48:03 -0300

    Editar
    Criar procedimentos para editar

--- gen/com/example/coolagenda/R.java
+++ gen/com/example/coolagenda/R.java
@@ -27,48 +27,64 @@ public final class R {
         public static final int logo=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f08001f;
+        public static final int action_settings=0x7f08002d;
+        public static final int btnCancelar=0x7f08001b;
         public static final int btnLogar=0x7f080003;
+        public static final int btnSalvar=0x7f08001a;
+        public static final int editEditaDataFinalCompromisso=0x7f080019;
+        public static final int editEditaDataInicialCompromisso=0x7f080017;
+        public static final int editEditaEmailContato=0x7f08001f;
+        public static final int editEditaEnderecoContato=0x7f080021;
+        public static final int editEditaNomeCompromisso=0x7f080015;
+        public static final int editEditaNomeContato=0x7f08001d;
         public static final int editLogin=0x7f080001;
         public static final int editSenha=0x7f080002;
         public static final int imageView1=0x7f080004;
         public static final int linearLayoutListCompromisso=0x7f08000a;
         public static final int linearLayoutListContato=0x7f080011;
-        public static final int listContatos=0x7f08001d;
-        public static final int listTarefa=0x7f08001e;
+        public static final int listContatos=0x7f08002b;
+        public static final int listTarefa=0x7f08002c;
         public static final int listaCompromissoActivity=0x7f08000b;
         public static final int listaContatoActivity=0x7f080012;
-        public static final int tableLayoutCompromisso=0x7f080014;
+        public static final int tableLayoutCompromisso=0x7f080022;
         public static final int tableLayoutCompromissoActivity=0x7f080005;
         public static final int tableLayoutContatoActivity=0x7f08000c;
         public static final int tableRowCompromissoActivity=0x7f080006;
         public static final int tableRowContatoActivity=0x7f08000d;
-        public static final int tbListaCompromisso=0x7f080015;
-        public static final int tbListaContato=0x7f080019;
+        public static final int tbListaCompromisso=0x7f080023;
+        public static final int tbListaContato=0x7f080027;
         public static final int text=0x7f080013;
         public static final int textView1=0x7f080000;
-        public static final int txtDataFinal=0x7f080018;
+        public static final int txtDataFinal=0x7f080026;
         public static final int txtDataFinalP=0x7f080009;
-        public static final int txtDataInicial=0x7f080017;
+        public static final int txtDataInicial=0x7f080025;
         public static final int txtDataInicialP=0x7f080008;
-        public static final int txtEmail=0x7f08001b;
+        public static final int txtEmail=0x7f080029;
         public static final int txtEmailP=0x7f08000f;
-        public static final int txtEndereco=0x7f08001c;
+        public static final int txtEndereco=0x7f08002a;
         public static final int txtEnderecoP=0x7f080010;
-        public static final int txtNome=0x7f080016;
-        public static final int txtNomeContato=0x7f08001a;
+        public static final int txtNome=0x7f080024;
+        public static final int txtNomeContato=0x7f080028;
         public static final int txtNomeContatoP=0x7f08000e;
         public static final int txtNomeP=0x7f080007;
+        public static final int txvEditaDataFinalCompromisso=0x7f080018;
+        public static final int txvEditaDataInicialCompromisso=0x7f080016;
+        public static final int txvEditaEmailContato=0x7f08001e;
+        public static final int txvEditaEnderecoContato=0x7f080020;
+        public static final int txvEditaNomeCompromisso=0x7f080014;
+        public static final int txvEditaNomeContato=0x7f08001c;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
         public static final int compromisso_activity=0x7f030001;
         public static final int contatos_activity=0x7f030002;
-        public static final int listacompromisso_activity=0x7f030003;
-        public static final int listacontatos_activity=0x7f030004;
-        public static final int listatarefa_activity=0x7f030005;
-        public static final int main_activity=0x7f030006;
-        public static final int tarefa_activity=0x7f030007;
+        public static final int editacompromisso_activity=0x7f030003;
+        public static final int editacontato_activity=0x7f030004;
+        public static final int listacompromisso_activity=0x7f030005;
+        public static final int listacontatos_activity=0x7f030006;
+        public static final int listatarefa_activity=0x7f030007;
+        public static final int main_activity=0x7f030008;
+        public static final int tarefa_activity=0x7f030009;
     }
     public static final class menu {
         public static final int login=0x7f070000;
@@ -77,13 +93,15 @@ public final class R {
         public static final int Coolagendatxt=0x7f050003;
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int cancelar=0x7f050008;
         public static final int hello_world=0x7f050001;
-        public static final int txtDataFinalP=0x7f05000a;
-        public static final int txtDataInicialP=0x7f050009;
+        public static final int salvar=0x7f050007;
+        public static final int txtDataFinalP=0x7f05000c;
+        public static final int txtDataInicialP=0x7f05000b;
         public static final int txtEmailP=0x7f050005;
         public static final int txtEnderecoP=0x7f050006;
-        public static final int txtIDP=0x7f050008;
-        public static final int txtNomeContatoP=0x7f050007;
+        public static final int txtIDP=0x7f05000a;
+        public static final int txtNomeContatoP=0x7f050009;
         public static final int txtNomeP=0x7f050004;
     }
     public static final class style {
